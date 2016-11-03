9d191a1eecda45d5b9b6af07db4060df9b18c3ad
Heber <hebercaballero@gmail.com>
2016-11-03 14:50:10 -0300

    Proyecto Inicial para el Desafio Recuperatorio

---
+++ CuentasBancarias/CuentasBancarias/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBoxCuentasBancarias = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.AgregarCuentaBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(107, 14)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(369, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(58, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Persona"
+        '
+        'ListBoxCuentasBancarias
+        '
+        Me.ListBoxCuentasBancarias.FormattingEnabled = True
+        Me.ListBoxCuentasBancarias.Location = New System.Drawing.Point(107, 39)
+        Me.ListBoxCuentasBancarias.Name = "ListBoxCuentasBancarias"
+        Me.ListBoxCuentasBancarias.Size = New System.Drawing.Size(369, 160)
+        Me.ListBoxCuentasBancarias.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Cuentas Bancarias"
+        '
+        'AgregarCuentaBtn
+        '
+        Me.AgregarCuentaBtn.Location = New System.Drawing.Point(7, 69)
+        Me.AgregarCuentaBtn.Name = "AgregarCuentaBtn"
+        Me.AgregarCuentaBtn.Size = New System.Drawing.Size(94, 46)
+        Me.AgregarCuentaBtn.TabIndex = 4
+        Me.AgregarCuentaBtn.Text = "Agregar Cuenta Bancaria ..."
+        Me.AgregarCuentaBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(488, 208)
+        Me.Controls.Add(Me.AgregarCuentaBtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBoxCuentasBancarias)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBoxCuentasBancarias As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents AgregarCuentaBtn As Button
+End Class
---
+++ CuentasBancarias/CuentasBancarias/Form2.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PersonaGBox = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.LblSexo = New System.Windows.Forms.Label()
+        Me.CmbSexo = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BancoLbl = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.CancelarCmd = New System.Windows.Forms.Button()
+        Me.AceptarCmd = New System.Windows.Forms.Button()
+        Me.PersonaGBox.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PersonaGBox
+        '
+        Me.PersonaGBox.Controls.Add(Me.Label4)
+        Me.PersonaGBox.Controls.Add(Me.TextBox4)
+        Me.PersonaGBox.Controls.Add(Me.Label3)
+        Me.PersonaGBox.Controls.Add(Me.TextBox3)
+        Me.PersonaGBox.Controls.Add(Me.LblSexo)
+        Me.PersonaGBox.Controls.Add(Me.CmbSexo)
+        Me.PersonaGBox.Controls.Add(Me.Label2)
+        Me.PersonaGBox.Controls.Add(Me.TextBox2)
+        Me.PersonaGBox.Controls.Add(Me.Label1)
+        Me.PersonaGBox.Controls.Add(Me.TextBox1)
+        Me.PersonaGBox.Location = New System.Drawing.Point(12, 12)
+        Me.PersonaGBox.Name = "PersonaGBox"
+        Me.PersonaGBox.Size = New System.Drawing.Size(386, 171)
+        Me.PersonaGBox.TabIndex = 0
+        Me.PersonaGBox.TabStop = False
+        Me.PersonaGBox.Text = "Persona"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(114, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(245, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Apellido y Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(77, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "CUIL"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(114, 49)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(93, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'LblSexo
+        '
+        Me.LblSexo.AutoSize = True
+        Me.LblSexo.Location = New System.Drawing.Point(77, 78)
+        Me.LblSexo.Name = "LblSexo"
+        Me.LblSexo.Size = New System.Drawing.Size(31, 13)
+        Me.LblSexo.TabIndex = 5
+        Me.LblSexo.Text = "Sexo"
+        '
+        'CmbSexo
+        '
+        Me.CmbSexo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CmbSexo.FormattingEnabled = True
+        Me.CmbSexo.Location = New System.Drawing.Point(114, 75)
+        Me.CmbSexo.Name = "CmbSexo"
+        Me.CmbSexo.Size = New System.Drawing.Size(100, 21)
+        Me.CmbSexo.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(56, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Dirección"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(114, 102)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(245, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 134)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Teléfono"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(114, 131)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(109, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ComboBox3)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.BancoLbl)
+        Me.GroupBox1.Controls.Add(Me.TextBox5)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.TextBox6)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 189)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(386, 160)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Cuenta Bancaria"
+        '
+        'BancoLbl
+        '
+        Me.BancoLbl.AutoSize = True
+        Me.BancoLbl.Location = New System.Drawing.Point(70, 22)
+        Me.BancoLbl.Name = "BancoLbl"
+        Me.BancoLbl.Size = New System.Drawing.Size(38, 13)
+        Me.BancoLbl.TabIndex = 7
+        Me.BancoLbl.Text = "Banco"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(114, 100)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(153, 20)
+        Me.TextBox5.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(76, 129)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(29, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "CBU"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(114, 126)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(191, 20)
+        Me.TextBox6.TabIndex = 4
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(114, 19)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox1.TabIndex = 8
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(114, 46)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(64, 21)
+        Me.ComboBox2.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(43, 49)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Tipo Cuenta"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(41, 103)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(64, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Nro. Cuenta"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(114, 73)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(64, 21)
+        Me.ComboBox3.TabIndex = 13
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(59, 76)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(46, 13)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Moneda"
+        '
+        'CancelarCmd
+        '
+        Me.CancelarCmd.Location = New System.Drawing.Point(321, 355)
+        Me.CancelarCmd.Name = "CancelarCmd"
+        Me.CancelarCmd.Size = New System.Drawing.Size(75, 23)
+        Me.CancelarCmd.TabIndex = 2
+        Me.CancelarCmd.Text = "Cancelar"
+        Me.CancelarCmd.UseVisualStyleBackColor = True
+        '
+        'AceptarCmd
+        '
+        Me.AceptarCmd.Location = New System.Drawing.Point(240, 355)
+        Me.AceptarCmd.Name = "AceptarCmd"
+        Me.AceptarCmd.Size = New System.Drawing.Size(75, 23)
+        Me.AceptarCmd.TabIndex = 3
+        Me.AceptarCmd.Text = "Aceptar"
+        Me.AceptarCmd.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(410, 385)
+        Me.Controls.Add(Me.AceptarCmd)
+        Me.Controls.Add(Me.CancelarCmd)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PersonaGBox)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.PersonaGBox.ResumeLayout(False)
+        Me.PersonaGBox.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PersonaGBox As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LblSexo As Label
+    Friend WithEvents CmbSexo As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents BancoLbl As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents CancelarCmd As Button
+    Friend WithEvents AceptarCmd As Button
+End Class
